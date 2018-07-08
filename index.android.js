2269adf9f26b5f6f97e4ff394c77e5529978e4c7
Lucas Francisco <lucasfrancisco1318@hotmail.com>
2018-07-08 08:45:57 -0300

    ok

---
+++ index.android.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+
+const app08 = props => (
+  <App />
+)
+
+AppRegistry.registerComponent('app08', () => app08);
